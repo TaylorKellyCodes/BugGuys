577ea09964ecb88802aa34a9bb065104ef000362
Taylor <Taylorkellycodes@gmail.com>
2026-02-13 07:57:10 -0500

    setup, added one offering page

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,23 +1,20 @@
 module.exports = {
-    name: "Code Stitch Web Designs",
-    email: "help@codestitch.app",
-    phoneForTel: "555-779-4407",
-    phoneFormatted: "(555) 779-4407",
+    name: "Bug Guys Pest Control",
+    email: "info@wekillthebugs.net",
+    phoneForTel: "919-630-8966",
+    phoneFormatted: "(919) 630-8966",
     address: {
-        lineOne: "First Address Line",
-        lineTwo: "Second Address Line",
-        city: "Denver",
-        state: "CO",
-        zip: "80206",
+        city: "Sanford",
+        state: "NC",
+        zip: "27332",
         country: "US",
-        mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
+        mapLink: "https://maps.app.goo.gl/J2YVX1KWUuF6hDH48",
     },
     socials: {
-        facebook: "https://www.facebook.com/",
-        instagram: "https://www.instagram.com/",
+        facebook: "https://www.facebook.com/WeKillTheBugs/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://www.example.com",
+    domain: "https://www.wekillthebugs.net",
     // Passing the isProduction variable for use in HTML templates
     isProduction: process.env.ELEVENTY_ENV === "PROD",
 };
